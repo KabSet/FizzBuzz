0ec329d643aebed06714a78390bd511c3e361242
Kabir Seth <kabset@dingo.zoo.lan>
2021-06-22 11:16:59 +0100

    third commit

--- app/src/main/java/com/example/fizzbuzz/FizzBuzz1.kt
+++ app/src/main/java/com/example/fizzbuzz/FizzBuzz1.kt
@@ -1,15 +1,25 @@
 package com.example.fizzbuzz
 
 fun main() {
-    for (x in 1..100) {
-        if (x % 3 == 0) {
-            print("Fizz")
-        }
-        if (x % 5 == 0) {
-            print("Buzz")
-        }
-        if (x % 3 != 0 && x % 5 != 0) {
-            print(x)
+    for (x in 1..195) {
+        if (x % 11 ==0) {
+            print("Bong")
+        } else {
+            if (x % 3 == 0) {
+                print("Fizz")
+            }
+            if (x % 13 == 0) {
+                print("Fezz")
+            }
+            if (x % 5 == 0) {
+                print("Buzz")
+            }
+            if (x % 7 == 0) {
+                print("Bang")
+            }
+            if (x % 3 != 0 && x % 5 != 0 && x % 7 != 0) {
+                print(x)
+            }
         }
         print("\n")
     }
